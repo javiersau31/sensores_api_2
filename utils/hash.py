2efed9358b8dc7a9306ea0c2a17ebc2aee100a48
Javier Saucedo <javisaucedoperez6@gmail.com>
2025-11-26 17:52:56 -0600

    hash act

--- utils/hash.py
+++ utils/hash.py
@@ -3,10 +3,10 @@ from passlib.context import CryptContext
 # Algoritmo recomendado para APIs
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str) -> str:
+def hash_password(password: str):
     """Devuelve la contraseña hasheada"""
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def verify_password(password: str, hashed_password: str) -> bool:
     """Verifica que la contraseña ingresada coincida con la almacenada"""
-    return pwd_context.verify(password, hashed_password)
+    return pwd_context.verify(password[:72], hashed_password)
